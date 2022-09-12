518902fc92e7016fb8d9668763b5e6dd7510078c
Samuel Kuta <kuta.samuel@gmail.com>
2022-09-12 09:04:04 +0200

    Add files via upload

---
+++ Csharp_Classes/Csharp_Classes/Line.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Classes
+{
+    internal class Line
+    {
+        private int length;
+
+        public Line(int length)
+        {
+            this.length = length;
+        }
+
+
+        public string draw()
+        {
+            String x = "";
+            while(x.Length < this.length)
+            {
+                x += "-";
+            }
+            return x;
+        }
+    }
+}
